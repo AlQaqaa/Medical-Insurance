0c74650469aa143c6c84e19027262f5f9500c6c2
Al-Qa'qaa Ali Benghuzi <che.ben.arg@hotmail.com>
2020-09-19 17:47:25 +0200

    refactor: Patient Processes Report

--- Medical Insurance/Reports/patientsExpenses.aspx.vb
+++ Medical Insurance/Reports/patientsExpenses.aspx.vb
@@ -25,7 +25,7 @@ Public Class patientsExpenses
     Private Sub getData()
         Try
             Dim sel_com As New SqlCommand("SELECT INC_Patient_Code, ISNULL(SUM(Processes_Price), 0) AS TOTAL_WITHDRAW, CARD_NO, NAME_ARB FROM INC_PATIANT
-            LEFT JOIN HAG_Processes ON Processes_Reservation_Code = INC_PATIANT.INC_Patient_Code AND HAG_Processes.Processes_Date BETWEEN (SELECT MAX(DATE_START) FROM INC_COMPANY_DETIAL WHERE INC_COMPANY_DETIAL.C_ID = INC_PATIANT.C_ID AND DATE_END > GETDATE()) AND GETDATE()
+            LEFT JOIN HAG_Processes ON Processes_Reservation_Code = INC_PATIANT.INC_Patient_Code AND Processes_State = 2 AND HAG_Processes.Processes_Date BETWEEN (SELECT MAX(DATE_START) FROM INC_COMPANY_DETIAL WHERE INC_COMPANY_DETIAL.C_ID = INC_PATIANT.C_ID AND DATE_END > GETDATE()) AND GETDATE()
             WHERE INC_Patient_Code IS NOT NULL AND INC_PATIANT.C_ID = " & ddl_companies.SelectedValue & "
             GROUP BY PINC_ID, INC_Patient_Code, CARD_NO, NAME_ARB", insurance_SQLcon)
             Dim dt_result As New DataTable
@@ -68,11 +68,59 @@ Public Class patientsExpenses
         If (e.CommandName = "printProcess") Then
             Dim index As Integer = Convert.ToInt32(e.CommandArgument)
             Dim row As GridViewRow = GridView1.Rows(index)
-            Dim p_link As String = "patientProcesses.aspx?pID=" & (row.Cells(0).Text)
-            Response.Write("<script type='text/javascript'>")
-            Response.Write("window.open('" & p_link & "','_blank');")
-            Response.Write("</script>")
-            'Response.Redirect("printPatientProcesses.aspx?invID=" & Val(txt_invoice_no.Text) & "&pID=" & (row.Cells(1).Text), False)
+
+            Dim sel_com As New SqlCommand("SELECT Processes_Reservation_Code, INC_PATIANT.NAME_ARB, INC_PATIANT.CARD_NO, INC_PATIANT.BAGE_NO, CONVERT(VARCHAR, INC_PATIANT.BIRTHDATE, 111) AS BIRTHDATE ,Processes_Price, Processes_Paid, Main_Clinic.Clinic_AR_Name ,Main_SubServices.SubService_AR_Name, CONVERT(VARCHAR, Processes_Date, 111) AS Processes_Date FROM HAG_Processes
+                LEFT JOIN Main_SubServices ON Main_SubServices.SubService_ID = HAG_Processes.Processes_SubServices
+                LEFT JOIN Main_Clinic ON Main_Clinic.clinic_id = HAG_Processes.Processes_Cilinc
+                LEFT JOIN INC_PATIANT ON INC_PATIANT.INC_Patient_Code = HAG_Processes.Processes_Reservation_Code
+                WHERE Processes_State = 2 AND Processes_Reservation_Code = '" & (row.Cells(0).Text) & "'", insurance_SQLcon)
+            insurance_SQLcon.Close()
+            insurance_SQLcon.Open()
+            main_ds.Tables("DataTable2").Load(sel_com.ExecuteReader)
+            insurance_SQLcon.Close()
+
+            Dim viewer As ReportViewer = New ReportViewer()
+            Dim datasource As New ReportDataSource("pateintDataSet", main_ds.Tables("DataTable2"))
+            viewer.LocalReport.DataSources.Clear()
+            viewer.ProcessingMode = ProcessingMode.Local
+            viewer.LocalReport.ReportPath = Server.MapPath("~/Reports/pateintProcesses.rdlc")
+            viewer.LocalReport.DataSources.Add(datasource)
+
+            Dim rp2 As ReportParameter
+            Dim rp3 As ReportParameter
+
+            rp2 = New ReportParameter("start_dt", ViewState("start_dt").ToString)
+            rp3 = New ReportParameter("user_name", Session("INC_User_name").ToString)
+
+            viewer.LocalReport.SetParameters(New ReportParameter() {rp2, rp3})
+
+            Dim rv As New Microsoft.Reporting.WebForms.ReportViewer
+            Dim r As String = "~/Reports/pateintProcesses.rdlc"
+            ' Page.Controls.Add(rv)
+
+            Dim warnings As Warning() = Nothing
+            Dim streamids As String() = Nothing
+            Dim mimeType As String = Nothing
+            Dim encoding As String = Nothing
+            Dim extension As String = Nothing
+            Dim bytes As Byte()
+            Dim FolderLocation As String
+            FolderLocation = Server.MapPath("~/Reports")
+            Dim filepath As String = FolderLocation & "/pateintProcesses" & Session("INC_User_Id") & ".pdf"
+            If Directory.Exists(filepath) Then
+                File.Delete(filepath)
+            End If
+            bytes = viewer.LocalReport.Render("PDF", Nothing, mimeType,
+                encoding, extension, streamids, warnings)
+            Dim fs As New FileStream(FolderLocation & "/pateintProcesses" & Session("INC_User_Id") & ".pdf", FileMode.Create)
+            fs.Write(bytes, 0, bytes.Length)
+            fs.Close()
+            Response.Redirect("~/Reports/pateintProcesses" & Session("INC_User_Id") & ".pdf", False)
+            'Dim p_link As String = "pateintProcesses" & Session("INC_User_Id") & ".pdf"
+            'Response.Write("<script type='text/javascript'>")
+            'Response.Write("window.open('" & p_link & "','_blank');")
+            'Response.Write("</script>")
+
         End If
     End Sub
 
--- WebSite/Reports/patientProcesses.aspx
+++
@@ -1,5 +0,0 @@
-﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/main.Master" CodeBehind="patientProcesses.aspx.vb" Inherits="Medical_Insurance.patientProcesses" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-</asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-</asp:Content>
